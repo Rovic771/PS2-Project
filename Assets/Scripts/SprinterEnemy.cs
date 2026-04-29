c6a8e44cd86f8706e5a584e7fe86cfcf1f87e993
Romain <rovic77@gmail.com>
2026-04-29 20:36:16 +0200

    Sprinter à terminé

--- Assets/Scripts/SprinterEnemy.cs
+++ Assets/Scripts/SprinterEnemy.cs
@@ -2,13 +2,46 @@ using UnityEngine;
 
 public class SprinterEnemy : Enemy
 {
+    [SerializeField] private float attackSpeed;
+    
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && !isStun && !TestIfWall())
+        {
+            playerDetected = true;
+        }
+        else playerDetected = false;
+    }
     public override void OnTriggerEnter2D(Collider2D other)
     {
-        throw new System.NotImplementedException();
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && !isStun && !TestIfWall())
+        {
+            playerDetected = true;
+        }
     }
 
     public override void OnTriggerExit2D(Collider2D other)
     {
-        throw new System.NotImplementedException();
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            playerDetected = false;
+        }
+    }
+
+    private void PursuitPlayer()
+    {
+        Debug.Log("PursuitPlayer");
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, attackSpeed * Time.deltaTime);
+    }
+
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        switch (playerDetected)
+        {
+            case true:
+                PursuitPlayer();
+                break;
+        }
     }
 }
