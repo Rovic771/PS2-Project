f154c510f303dba594981add16ef3b940bf992e0
DracoMasque <aksel.boulongne@gmail.com>
2026-05-03 10:24:01 +0200

    zone fix
    zone

--- Assets/Scripts/ZonePlayer.cs
+++ Assets/Scripts/ZonePlayer.cs
@@ -9,14 +9,14 @@ public class ZonePlayer : MonoBehaviour
     {
         if (zoneActiveSys.zoneActive)
         {
-            if (other.gameObject.layer == LayerMask.NameToLayer("ReObject"))
+            if (gameObject.CompareTag("DoZone") && other.gameObject.layer == LayerMask.NameToLayer("DoObject"))
             {
                 Destroy(other.gameObject);
             }
         }
         else 
         {
-            if (other.gameObject.layer == LayerMask.NameToLayer("DoObject"))
+            if (gameObject.CompareTag("ReZone") && other.gameObject.layer == LayerMask.NameToLayer("ReObject"))
             {
                 Destroy(other.gameObject);
             }
