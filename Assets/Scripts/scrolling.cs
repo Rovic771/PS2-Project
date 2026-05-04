d898f7c8b8f5662f2d0a0f904590ae872013989e
Romain Victor <rovic77@gmail.com>
2026-05-04 15:28:33 +0200

    Scrolling nuage

---
+++ Assets/Scripts/scrolling.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class scrolling : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 endScrolling;
+    private Vector2 posInit;
+    
+    void Start()
+    {
+        posInit = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Vector2.Distance(transform.position, endScrolling) < 1f)
+        {
+            transform.position = posInit;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, endScrolling, speed * Time.deltaTime);
+    }
+}
