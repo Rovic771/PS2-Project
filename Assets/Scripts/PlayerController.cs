b9ff5ee6891397aec3bbfb7da2b8f2fe0bcfed6c
Aksel BOULONGNE <aksel.boulongne@gmail.com>
2026-05-18 15:03:28 +0200

    changement ld

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using NUnit.Framework;
 using Unity.VisualScripting;
-using UnityEditor.Rendering;
+//using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
