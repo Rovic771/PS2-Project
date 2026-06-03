1a9763be10b5328f9bcc6975c6b7ee309a481c3a
Aksel BOULONGNE <aksel.boulongne@gmail.com>
2026-06-03 16:38:34 +0200

    menu quasi fini + 2-3fix dans good assanblage 1

--- Assets/Scripts/sliderVolume.cs
+++ Assets/Scripts/sliderVolume.cs
@@ -5,8 +5,19 @@ public class sliderVolume : MonoBehaviour
 {
     [SerializeField] private AudioMixer mixer;
 
+    private void Start()
+    {
+        mixer.SetFloat("Volume", Mathf.Log10(PlayerPrefs.GetFloat("Volume",1)) * 20);
+    }
+
     public void OnChangeSlider(float value)
     {
         mixer.SetFloat("Volume", Mathf.Log10(value) * 20);
+        if (value == 0)
+        {
+            mixer.SetFloat("Volume", -80);
+        }
+        PlayerPrefs.SetFloat("Volume", value);
+        PlayerPrefs.Save();
     }
 }
