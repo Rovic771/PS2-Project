c83988d66016cde67acf627147a1a3bfd0e60bce
Romain <rovic77@gmail.com>
2026-06-07 14:52:32 +0200

    Reste à ajouter l'audio de la marche

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -16,7 +16,7 @@ public class Checkpoint : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //AudioManager.Instance.SoundExample(9);
+        AudioManager.Instance.PlaySound(0, 9, AudioManager.Sound.checkpoint, gameObject);
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             PlayerPrefs.SetFloat("checkpointX", transform.position.x);
